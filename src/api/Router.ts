12696298645f9e41a77a0c496ad1afefd95ef0c2
Guilherme Castro <guii_1@hotmail.com>
2024-04-11 12:46:34 -0300

    create router entity

---
+++ src/api/Router.ts
@@ -0,0 +1,79 @@
+import { regionsController, userController } from '../bootstrap';
+import { HttpServer } from './HttpServer';
+
+export class Router {
+  private server: HttpServer;
+  constructor(server: HttpServer) {
+    this.server = server;
+  }
+
+  init() {
+    this.server.on({
+      path: '',
+      method: 'get',
+      handler: async () => {
+        return { body: 'Funcionando', statusCode: 200 };
+      },
+    });
+
+    this.server.on({
+      path: '/user/create',
+      method: 'post',
+      handler: userController.register,
+    });
+
+    this.server.on({
+      path: '/user/get-all',
+      method: 'get',
+      handler: userController.getAll,
+    });
+
+    this.server.on({
+      path: '/user/get-one/:user_id',
+      method: 'get',
+      handler: userController.getById,
+    });
+
+    this.server.on({
+      path: '/user/update/:user_id',
+      method: 'put',
+      handler: userController.update,
+    });
+
+    this.server.on({
+      path: '/user/delete/:user_id',
+      method: 'delete',
+      handler: userController.delete,
+    });
+
+    this.server.on({
+      path: '/regions/create',
+      method: 'post',
+      handler: regionsController.create,
+    });
+
+    this.server.on({
+      path: '/regions/get-all',
+      method: 'get',
+      handler: regionsController.getAll,
+    });
+
+    this.server.on({
+      path: '/regions/get-one/:regions_id',
+      method: 'get',
+      handler: regionsController.getById,
+    });
+
+    this.server.on({
+      path: '/regions/update/:regions_id',
+      method: 'put',
+      handler: regionsController.update,
+    });
+
+    this.server.on({
+      path: '/regions/delete/:regions_id',
+      method: 'delete',
+      handler: regionsController.delete,
+    });
+  }
+}
