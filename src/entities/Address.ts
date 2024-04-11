223c796f277e4f5f301019035c93958e508621f4
Guilherme Castro <guii_1@hotmail.com>
2024-04-11 12:39:20 -0300

    create adresses entity and types

---
+++ src/entities/Address.ts
@@ -0,0 +1,41 @@
+export interface CoordinatesAttributes {
+  latitude: number;
+  longitude: number;
+}
+
+export interface AddressAttributes {
+  street: string;
+  city: string;
+  state: string;
+  country: string;
+  zipCode: string;
+}
+
+export class Coordinates {
+  latitude: number;
+  longitude: number;
+  constructor(attributes: CoordinatesAttributes) {
+    this.latitude = attributes.latitude;
+    this.longitude = attributes.longitude;
+  }
+}
+
+export class Address {
+  street: string;
+  city: string;
+  state: string;
+  country: string;
+  zipCode: string;
+  constructor(attributes: AddressAttributes) {
+    this.city = attributes.city;
+    this.state = attributes.state;
+    this.street = attributes.street;
+    this.country = attributes.country;
+    this.zipCode = attributes.zipCode;
+  }
+}
+
+export interface GeoLocationService {
+  getAddressFromCoordinates(coordinates: Coordinates): Promise<Address>;
+  getCoordinatesFromAddress(address: Address): Promise<Coordinates>;
+}
