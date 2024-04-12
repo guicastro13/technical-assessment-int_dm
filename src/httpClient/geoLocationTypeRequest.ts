7b655d3bf5f09fbe689adf75381b3a9cfa64e3e3
Guilherme Castro <guii_1@hotmail.com>
2024-04-12 00:28:09 -0300

    create service to get Geolocation by address and coordinate

---
+++ src/httpClient/geoLocationTypeRequest.ts
@@ -0,0 +1,59 @@
+export interface ResponseData {
+    items: Item[];
+}
+
+interface Address {
+    label: string;
+    countryCode: string;
+    countryName: string;
+    stateCode: string;
+    state: string;
+    county: string;
+    city: string;
+    district: string;
+    street: string;
+    postalCode: string;
+    houseNumber: string;
+}
+
+interface Position {
+    lat: number;
+    lng: number;
+}
+
+interface Access {
+    lat: number;
+    lng: number;
+}
+
+
+interface MapView {
+    west: number;
+    south: number;
+    east: number;
+    north: number;
+}
+
+interface FieldScore {
+    city: number;
+    streets: number[];
+    houseNumber: number;
+}
+
+interface Scoring {
+    queryScore: number;
+    fieldScore: FieldScore;
+}
+
+interface Item {
+    title: string;
+    id: string;
+    resultType: string;
+    houseNumberType: string;
+    address: Address;
+    position: Position;
+    access: Access[];
+    mapView: MapView;
+    scoring: Scoring;
+}
+
---
+++ src/service/GeoLocationService.ts
@@ -0,0 +1,35 @@
+import { Address, Coordinates, GeoLocationService } from '../entities/Address';
+import { HttpError } from '../errors/HttpError';
+import { HttpClient } from '../httpClient/Axios';
+import { ResponseData } from '../httpClient/geoLocationTypeRequest';
+
+export class GoogleMapsGeoLocationService implements GeoLocationService {
+  private _apiKey = "iKetBAF352CCNoMYmLlrygt6jA-JJf3_RuuNtlJBlFA"
+  private baseUrlAddress = "https://geocode.search.hereapi.com/v1/geocode"
+  private baseUrlCordenates = "https://revgeocode.search.hereapi.com/v1/revgeocode"
+
+  constructor(private axios: HttpClient) {}
+  
+  async getAddressFromCoordinates(coordinates: Coordinates): Promise<Address> {
+    if (!coordinates) throw new HttpError("Should have to contain coordinates to transform in address")
+    const { latitude, longitude } = coordinates
+    const result = await this.axios.get<ResponseData>(`${this.baseUrlCordenates}?at=${latitude}%2C${longitude}&lang=en-US&apiKey=${this._apiKey}`)
+    return {
+      city:result.items[0].address.city,
+      country: result.items[0].address.county,
+      state: result.items[0].address.state,
+      street: result.items[0].address.street,
+      zipCode: result.items[0].address.postalCode
+    } as Address;
+  }
+
+  async getCoordinatesFromAddress(address: Address): Promise<Coordinates> {
+    if (!address) throw new HttpError("Should have to contain address to transform in coordinates")
+    const { city, country, state, street, zipCode} = address
+    const result = await this.axios.get<ResponseData>(`${this.baseUrlAddress}?q=+${city}+${country}+${state}+${street}+${zipCode}&apiKey=${this._apiKey}`)
+    return {
+      latitude: result.items[0].access[0].lat,
+      longitude: result.items[0].access[0].lng
+    } as Coordinates;
+  }
+}
