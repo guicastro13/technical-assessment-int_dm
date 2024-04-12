0276f4bc5364e26a7deb7167b9dc4f1dc9203742
Guilherme Castro <guii_1@hotmail.com>
2024-04-12 00:27:38 -0300

    add axios to projetc

---
+++ src/httpClient/Axios.ts
@@ -0,0 +1,18 @@
+import axios, { AxiosRequestConfig, CreateAxiosDefaults } from "axios";
+
+export interface HttpClient {
+  get: <R>(url: string, options?: AxiosRequestConfig) => Promise<R>
+  post: <R>(url: string, body: any, options?: any) => Promise<R>
+}
+
+export class AxiosAdapter implements HttpClient {
+  async get<R>(url: string, options?: AxiosRequestConfig) {
+    const response = await axios.get<R>(url, options);
+    return response.data;
+  }
+
+  async post<R>(url: string, body: any, options?: AxiosRequestConfig) {
+    const response = await axios.post<R>(url, body, options);
+    return response.data;
+  }
+}
