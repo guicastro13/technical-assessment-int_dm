93b881fcfb79305cc39e54f62d6907d53b49c669
Guilherme Castro <guii_1@hotmail.com>
2024-04-12 21:58:14 -0300

    create get all regions close to coordinate

---
+++ src/service/regions/GetAllRegionsCloseToCoordinate.ts
@@ -0,0 +1,27 @@
+import { RegionsRepositoryI } from "../../database/repositories/RegionsRepositoryInterface";
+import { Coordinates } from "../../entities/Address";
+import { Region } from "../../entities/Region";
+import { GeoLocationDistance } from "../GeoLocationDistance";
+
+export class GetAllRegionsCloseToCoordinate {
+    constructor(
+        private regionsRepo: RegionsRepositoryI,
+        private geoLocationDistance: GeoLocationDistance
+    ) {}
+
+    async exec(coordinates: Coordinates, howMuchCloseInKM?: number) {
+        const regions = await this.regionsRepo.getAll();
+        if (!regions) return [];
+
+        const closeRegions: Array<{ region: Region, distance: number} | []> = [];
+
+        regions.forEach((region) => {
+            const distance = this.geoLocationDistance.getDistance(coordinates, region.coordinates);
+            if (howMuchCloseInKM === undefined || distance <= howMuchCloseInKM) {
+                closeRegions.push({ region, distance });
+            }
+        });
+
+        return closeRegions;
+    }
+}
