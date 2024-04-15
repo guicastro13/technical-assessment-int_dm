0ed54da4cca2aa5be527adcc16e0c228df7f65bf
Guilherme Castro <guii_1@hotmail.com>
2024-04-15 09:13:07 -0300

    create GetRegionsCloseToCoordinate nad Different to user Id given

---
+++ src/service/regions/GetRegionsCloseToCoordinateDiffUsarId.ts
@@ -0,0 +1,28 @@
+import { RegionsRepositoryI } from '../../database/repositories/RegionsRepositoryInterface';
+import { Coordinates } from '../../entities/Address';
+import { Region } from '../../entities/Region';
+import { GeoLocationDistance } from '../GeoLocationDistance';
+
+export class GetRegionsCloseToCoordinateDiffUserId {
+  constructor(
+    private regionsRepo: RegionsRepositoryI,
+    private geoLocationDistance: GeoLocationDistance,
+  ) {}
+
+  async exec(coordinates: Coordinates, userId: string, howMuchCloseInKM?: number) {
+    if (!userId) return [];
+    const regions = await this.regionsRepo.getAll();
+    if (!regions) return [];
+
+    const closeRegions: Array<{ region: Region; distance: number }> = [];
+
+    regions.forEach((region) => {
+      const distance = this.geoLocationDistance.getDistance(coordinates, region.coordinates);
+      if ((howMuchCloseInKM === undefined || distance <= howMuchCloseInKM) && region.userId != userId) {
+        closeRegions.push({ region, distance });
+      }
+    });
+
+    return closeRegions;
+  }
+}
