6420da0a79f6d9553051a01d1d60aea28f9a6614
Guilherme Castro <guii_1@hotmail.com>
2024-04-15 20:17:36 -0300

    create entity mongo

---
+++ src/database/mongodb/mongodb.ts
@@ -0,0 +1,23 @@
+import { Database } from '../database';
+
+const mongoose = require('mongoose');
+
+export class MongoDB implements Database {
+  private uri;
+  constructor(uri: string) {
+    this.uri = uri;
+  }
+
+  async connect() {
+    try {
+      await mongoose.connect(this.uri);
+      console.log('Conectado ao banco de dados MongoDB');
+    } catch (err) {
+      console.error('Erro na conexão com o banco de dados:', err);
+    }
+  }
+
+  close() {
+    mongoose.connection.close();
+  }
+}
