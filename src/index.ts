7c170d0391784968dd3a1e3d8aec6331c18d0499
Guilherme Castro <guii_1@hotmail.com>
2024-04-15 20:18:01 -0300

    adding connector to db inicialize

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { ExpressAdapter } from './api/ExpressAdapter';
 import { Router } from './api/Router';
+import { MongoDB } from './database/mongodb/mongodb';
+
+new MongoDB('mongodb://localhost:27017/techinical_test').connect();
 
 const server = new ExpressAdapter();
 const router = new Router(server);
