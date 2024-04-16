96010d2e4e675486765103a73e6f7bd50c5caec2
Guilherme Castro <guii_1@hotmail.com>
2024-04-16 18:26:40 -0300

    create dependncy to inicialize server, to wait mongodb

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,16 @@ import { ExpressAdapter } from './api/ExpressAdapter';
 import { Router } from './api/Router';
 import { MongoDB } from './database/mongodb/mongodb';
 
-new MongoDB('mongodb://localhost:27017/techinical_test').connect();
+async function initialize() {
+    const mongoDB = new MongoDB('mongodb://mongodb:27017/techinical_test');
+    await mongoDB.connect();
 
-const server = new ExpressAdapter();
-const router = new Router(server);
-router.init();
-server.start(3000);
+    const server = new ExpressAdapter();
+    const router = new Router(server);
+    router.init();
+    server.start(3000);
+}
+
+initialize().catch(err => {
+    console.error('error when initializing: ', err);
+});
