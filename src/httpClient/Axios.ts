f5a95bd2c1341296f58b458c8d70196b34f0adbb
Guilherme Castro <guii_1@hotmail.com>
2024-04-17 09:49:14 -0300

    add logger and possibly null

--- src/httpClient/Axios.ts
+++ src/httpClient/Axios.ts
@@ -1,18 +1,32 @@
-import axios, { AxiosRequestConfig, CreateAxiosDefaults } from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
+import { LoggerI } from '../helpers/Logger';
+
 
 export interface HttpClient {
-  get: <R>(url: string, options?: AxiosRequestConfig) => Promise<R>;
-  post: <R>(url: string, body: any, options?: any) => Promise<R>;
+  get: <R>(url: string, options?: AxiosRequestConfig) => Promise<R | null>;
+  post: <R>(url: string, body: any, options?: AxiosRequestConfig) => Promise<R | null>;
 }
 
 export class AxiosAdapter implements HttpClient {
-  async get<R>(url: string, options?: AxiosRequestConfig) {
-    const response = await axios.get<R>(url, options);
-    return response.data;
+  constructor(private logger: LoggerI) {}
+
+  async get<R>(url: string, options?: AxiosRequestConfig): Promise<R | null> {
+      try {
+          const response = await axios.get<R>(url, options);
+          return response.data;
+      } catch {
+          this.logger.error('Erro ao realizar a solicitação GET');
+          return null;
+      }
   }
 
-  async post<R>(url: string, body: any, options?: AxiosRequestConfig) {
-    const response = await axios.post<R>(url, body, options);
-    return response.data;
+  async post<R>(url: string, body: any, options?: AxiosRequestConfig): Promise<R | null> {
+      try {
+          const response = await axios.post<R>(url, body, options);
+          return response.data;
+      } catch {
+          this.logger.error('Erro ao realizar a solicitação POST');
+          return null;
+      }
   }
 }
