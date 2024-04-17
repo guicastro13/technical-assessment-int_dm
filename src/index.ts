abc772a92ccdafa9f875c895aba732e849d0bd90
Guilherme Castro <guii_1@hotmail.com>
2024-04-17 09:51:19 -0300

    refac server init

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,3 @@
-import { ExpressAdapter } from './api/ExpressAdapter';
-import { Router } from './api/Router';
-import { MongoDB } from './database/mongodb/mongodb';
+import { server } from "./bootstrap";
 
-async function initialize() {
-    const mongoDB = new MongoDB('mongodb://mongodb:27017/techinical_test');
-    await mongoDB.connect();
-
-    const server = new ExpressAdapter();
-    const router = new Router(server);
-    router.init();
-    server.start(3000);
-}
-
-initialize().catch(err => {
-    console.error('error when initializing: ', err);
-});
+server.init()
