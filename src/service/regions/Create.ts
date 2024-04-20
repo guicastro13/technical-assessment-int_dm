0275dfc4c8520ddca89b66cc23b80809bfffda3b
Guilherme Castro <guii_1@hotmail.com>
2024-04-20 19:00:50 -0300

    add user ID verification

--- src/service/regions/Create.ts
+++ src/service/regions/Create.ts
@@ -1,10 +1,18 @@
 import { RegionsRepositoryI } from '../../database/repositoriesInterfaces/RegionsRepositoryInterface';
 import { Region, RegionAttributes } from '../../entities/Region';
+import { Conflict } from '../../errors/Conflict';
+import { GetUserById } from '../user/GetUserById';
 
 export class CreateRegion {
-  constructor(private regionsRepo: RegionsRepositoryI) {}
+  constructor(
+    private regionsRepo: RegionsRepositoryI,
+    private getUserById: GetUserById
+  ) {}
 
   async exec(attributes: RegionAttributes) {
+    const user = await this.getUserById.exec(attributes.userId)
+    if(!user) throw new Conflict("Não foi encontrado nenhum USER o ID forncenido")
+
     const region = new Region(attributes);
     return this.regionsRepo.save(region);
   }
