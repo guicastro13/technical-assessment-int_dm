5c60b4513cbb1802d898e8208874577f84eef774
Guilherme Castro <guii_1@hotmail.com>
2024-04-20 18:59:48 -0300

    add id and email verification

--- src/service/user/Create.ts
+++ src/service/user/Create.ts
@@ -1,10 +1,21 @@
 import { UsersRepositoryI } from '../../database/repositoriesInterfaces/UsersRepositoryInterface';
 import { User, UserAttributes } from '../../entities/User';
+import { Conflict } from '../../errors/Conflict';
 
 export class CreateUser {
   constructor(private usersRepo: UsersRepositoryI) {}
 
   async exec(attributes: UserAttributes) {
+    if (attributes.id) {
+      const hasUserWithId = await this.usersRepo.getUserById(attributes.id)
+      if(hasUserWithId) throw new Conflict("Já existe um úsuario com esse ID")
+    }
+
+    if (attributes.email) {
+      const hasUserWithId = await this.usersRepo.getUserByEmail(attributes.email)
+      if(hasUserWithId) throw new Conflict("Já existe um úsuario com esse EMAIL")
+    }
+
     const user = new User(attributes);
     return this.usersRepo.save(user);
   }
