087dc151d407ee93ea3dd21a26e0140aca82b406
Guilherme Castro <guii_1@hotmail.com>
2024-04-22 17:53:49 -0300

    add service to resolve address <-> coordenate

--- src/service/user/Create.ts
+++ src/service/user/Create.ts
@@ -1,9 +1,13 @@
 import { UsersRepositoryI } from '../../database/repositoriesInterfaces/UsersRepositoryInterface';
+import { Address, Coordinates, GeoLocationService } from '../../entities/Address';
 import { User, UserAttributes } from '../../entities/User';
 import { Conflict } from '../../errors/Conflict';
 
 export class CreateUser {
-  constructor(private usersRepo: UsersRepositoryI) {}
+  constructor(
+    private usersRepo: UsersRepositoryI,
+    private geoLocation: GeoLocationService,
+  ) {}
 
   async exec(attributes: UserAttributes) {
     if (attributes.id) {
@@ -14,6 +18,12 @@ export class CreateUser {
     if (attributes.email) {
       const hasUserWithId = await this.usersRepo.getUserByEmail(attributes.email);
       if (hasUserWithId) throw new Conflict('Já existe um úsuario com esse EMAIL');
+    }
+
+    if (attributes.coordinates) {
+      attributes.address = await this.geoLocation.getAddressFromCoordinates(attributes.coordinates);
+    } else if (attributes.address) {
+      attributes.coordinates = await this.geoLocation.getCoordinatesFromAddress(attributes.address);
     }
 
     const user = new User(attributes);
--- src/service/user/Update.ts
+++ src/service/user/Update.ts
@@ -1,22 +1,34 @@
 import { UsersRepositoryI } from '../../database/repositoriesInterfaces/UsersRepositoryInterface';
+import { GeoLocationService } from '../../entities/Address';
 import { User, UserAttributes, UserAttributesUpdate } from '../../entities/User';
 import { Conflict } from '../../errors/Conflict';
 
 export class UpdaterUser {
-  constructor(private usersRepo: UsersRepositoryI) {}
+  constructor(
+    private usersRepo: UsersRepositoryI,
+    private geoLocation: GeoLocationService,
+  ) {}
 
   async exec(userId: string, updatederUser: UserAttributesUpdate): Promise<User | null> {
     const user = await this.usersRepo.getUserById(userId);
     if (user === null) throw new Conflict('Nenhum usuário encontrado com esse ID');
-    const { address, coordinates, email, name } = updatederUser;
-    const newUser = new User({
-      id: userId,
-      name: name ? name : user.name,
-      email: email ? email : user.email,
-      coordinates: coordinates ? coordinates : user.coordinates,
-      address: address ? address : user.address,
+
+    if (updatederUser.coordinates) {
+      updatederUser.address = await this.geoLocation.getAddressFromCoordinates(updatederUser.coordinates);
+    } else if (updatederUser.address) {
+      updatederUser.coordinates = await this.geoLocation.getCoordinatesFromAddress(updatederUser.address);
+    }
+
+    const newUserAttributes: UserAttributes = {
+      id: user.id,
+      name: updatederUser.name ?? user.name,
+      email: updatederUser.email ?? user.email,
+      address: updatederUser.address,
+      coordinates: updatederUser.coordinates,
+      createdAt: user.createdAt,
       updatedAt: new Date(),
-    });
+    };
+    const newUser = new User(newUserAttributes);
     return this.usersRepo.updateById(userId, newUser);
   }
 }
