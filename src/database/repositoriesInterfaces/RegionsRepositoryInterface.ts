6cf8218f8d07a5d3acc364de0e3d9e0c6b4c91e0
Guilherme Castro <guii_1@hotmail.com>
2024-04-22 18:00:59 -0300

    add on interface getRegionsNearby

--- src/database/repositoriesInterfaces/RegionsRepositoryInterface.ts
+++ src/database/repositoriesInterfaces/RegionsRepositoryInterface.ts
@@ -1,3 +1,4 @@
+import { Coordinates } from '../../entities/Address';
 import { Region } from '../../entities/Region';
 
 export interface RegionsRepositoryI {
@@ -6,4 +7,5 @@ export interface RegionsRepositoryI {
   getRegionById(regionId: string): Promise<Region | null>;
   updateById(regionId: string, region: Region): Promise<Region | null>;
   deleteById(regionId: string): Promise<void>;
+  getRegionsNearby(coordinates: Coordinates, howMuchCloseInKM: number, userId?: string): Promise<Array<Region> | null>;
 }
