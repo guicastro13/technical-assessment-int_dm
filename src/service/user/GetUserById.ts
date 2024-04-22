61ce06bce634c4de78d30c491991d2b03a23d4bc
Guilherme Castro <guii_1@hotmail.com>
2024-04-22 17:54:26 -0300

    add throwerror, when id no found

--- src/service/user/GetUserById.ts
+++ src/service/user/GetUserById.ts
@@ -1,10 +1,13 @@
 import { UsersRepositoryI } from '../../database/repositoriesInterfaces/UsersRepositoryInterface';
 import { User } from '../../entities/User';
+import { Conflict } from '../../errors/Conflict';
 
 export class GetUserById {
   constructor(private usersRepo: UsersRepositoryI) {}
 
   async exec(userId: string): Promise<User | null> {
+    const user = await this.usersRepo.getUserById(userId);
+    if (user === null) throw new Conflict('Nenhum usuário encontrado com esse ID');
     return this.usersRepo.getUserById(userId);
   }
 }
